8dd6a188a560ca69d800fedd9f10907d0bc6b9f6
Marlon Ferreira <marlonferreira@hotmail.com>
2021-02-22 12:54:59 -0300

    Ajuste width lg

--- src/components/atoms/Container.js
+++ src/components/atoms/Container.js
@@ -11,8 +11,8 @@ const Container = styled.div`
   }
 
   ${breakAt(BreakpointsSizes.lg)} {
-    /* width: 1140px; */
-    width: 950px;
+    width: 1140px;
+    /* width: 950px; */
     /* width: 100%; */
     padding: 0 16px;
     /* padding: 0; */
