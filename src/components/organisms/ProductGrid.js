857a20cd6ac980dd491cc5ba9ff91b410bcd161b
Marlon Ferreira <marlonferreira@hotmail.com>
2021-02-24 11:50:07 -0300

    Update home page

--- src/components/organisms/ProductGrid.js
+++ src/components/organisms/ProductGrid.js
@@ -12,6 +12,18 @@ const Toolbar = styled.div`
   text-align: center;
 `;
 
+// Para fazer todos os cards terem a mesma altura.
+// Descomentar o conteudo do styled abaixo e adicionar min-height no StyledCard do Card.js
+const Adjustment = styled.div`
+  /* position: relative;
+  height: 500px; */
+
+  Button {
+    /* position: absolute;
+    bottom: 0; */
+  }
+`;
+
 const ProductGrid = ({ products }) => {
   const [showAll, setShowAll] = useState(false);
   const filteredProducts = showAll ? products : products.slice(0, 3);
@@ -20,20 +32,22 @@ const ProductGrid = ({ products }) => {
     <>
       <Grid md={3}>
         {filteredProducts.map((product) => (
-          <Card key={product.id}>
-            <CardMedia image={product.image} />
-            <CardBody>
-              <Heading>
-                <h6>{product.title}</h6>
-              </Heading>
-              <p>{product.summary}</p>
-              <div>
-                <Button color="primary" variant="link">
-                  Saiba mais
-                </Button>
-              </div>
-            </CardBody>
-          </Card>
+          <Adjustment key={product.id}>
+            <Card>
+              <CardMedia image={product.image} />
+              <CardBody>
+                <Heading>
+                  <h6>{product.title}</h6>
+                </Heading>
+                <p>{product.summary}</p>
+                <div>
+                  <Button color="primary" variant="link">
+                    Saiba mais
+                  </Button>
+                </div>
+              </CardBody>
+            </Card>
+          </Adjustment>
         ))}
       </Grid>
       {!showAll && (
