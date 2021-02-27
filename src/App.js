9195eb026f224c47495d7afc238d484f31355e57
Marlon Ferreira <marlonferreira@hotmail.com>
2021-02-26 21:38:09 -0300

    Create routes directory

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import GlobalStyle from "styles/GlobalStyle";
 import ThemeProvider from "styles/ThemeProvider";
-
-import Home from "components/pages/Home";
-import About from "components/pages/About";
-import ProductDetail from "components/pages/ProductDetail";
+import Routes from "routes";
 
 function App() {
   return (
     <ThemeProvider>
       <GlobalStyle />
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre" element={<About />} />
-          <Route path="/servicos" element={<ProductDetail />} />
-        </Routes>
+        <Routes />
       </Router>
     </ThemeProvider>
   );
